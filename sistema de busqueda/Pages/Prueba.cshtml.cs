ad147ac698c1b9c6ce0641b1cf3992b6f60885c8
JUAN EDUARDO SEPULVEDA MONARES <JUAN.SEPULVEDAMONA@correoaiep.cl>
2021-09-22 21:46:49 -0300

    Agregar archivos de proyecto.

---
+++ sistema de busqueda/Pages/Prueba.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace sistema_de_busqueda.Pages
+{
+    public class PruebaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
