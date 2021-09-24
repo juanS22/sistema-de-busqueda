2e80beca1d446061dc714bcff0cd0ae356b69cc5
JUAN EDUARDO SEPULVEDA MONARES <JUAN.SEPULVEDAMONA@correoaiep.cl>
2021-09-24 01:20:28 -0300

    2409120

--- Sistemadebusqueda2/Sistemadebusqueda2/Pages/Index.cshtml.cs
+++ Sistemadebusqueda2/Sistemadebusqueda2/Pages/Index.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Sistemadebusqueda2.Repositories;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +12,14 @@ namespace Sistemadebusqueda2.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        [Required(ErrorMessage ="El campo usuario es requerido")]
+        public string Usuario { get; set; }
+        [Display(Name="Contraseña")]
+        [BindProperty]
+        [Required(ErrorMessage ="El campo contraseña es requerido")]
+        public string Password { get; set; }
+
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -19,6 +29,34 @@ namespace Sistemadebusqueda2.Pages
 
         public void OnGet()
         {
+
+        }
+
+        public ActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var usuario = this.Usuario;
+                var pass = this.Password;
+                var repo = new IndexRepository();
+
+                if (repo.ValidarUsuario(usuario, pass))
+                {
+                    return RedirectToPage("./Prueba");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "El usuario o la contraseña no son validos");
+                    return Page();
+                }
+                    
+
+            }
+            else
+            {
+                return Page();
+            }
+            
 
         }
     }
