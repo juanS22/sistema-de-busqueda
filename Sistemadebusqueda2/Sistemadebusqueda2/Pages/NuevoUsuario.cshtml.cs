1944e77303232c4e566b20f99a59987b92532a9c
JUAN EDUARDO SEPULVEDA MONARES <JUAN.SEPULVEDAMONA@correoaiep.cl>
2021-09-24 05:10:38 -0300

    2409510

--- Sistemadebusqueda2/Sistemadebusqueda2/Pages/NuevoUsuario.cshtml.cs
+++ Sistemadebusqueda2/Sistemadebusqueda2/Pages/NuevoUsuario.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -10,6 +11,32 @@ namespace Sistemadebusqueda2.Pages
 {
     public class NuevoUsuarioModel : PageModel
     {
+        [BindProperty]        
+        [Display (Name ="Nombre de Usuario")]
+        [Required(ErrorMessage = "El campo nombre de usuario es requerido")]
+        public string NombreUsuario { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "El campo nombres es requerido")]
+        public string Nombres { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "El campo apellidos es requerido")]
+        public string Apellidos { get; set; }
+        [BindProperty]        
+        [Display(Name = "Rol")]
+        [Required(ErrorMessage = "El campo Rol es requerido")]
+        public int? RolId { get; set; }
+        [BindProperty]        
+        [Display(Name = "Contraseña")]
+        [Required(ErrorMessage = "El campo contraseña es requerido")]
+        [MinLength(8,ErrorMessage ="La contraseña debe tener al menos 8 caracteres")]
+        [RegularExpression("^(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,16}$", ErrorMessage ="La contraseña debe tener al menos una mayuscula, minusculas y digitos")]
+        public string Password { get; set; }
+        [BindProperty]       
+        [Display(Name = "Repetir Contraseña")]
+        [Required(ErrorMessage = "El campo repetir contraseña es requerido")]
+        [MinLength(8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres")]
+        [RegularExpression("^(?=\\w*\\d)(?=\\w*[A-Z])(?=\\w*[a-z])\\S{8,16}$", ErrorMessage = "La contraseña debe tener al menos una mayuscula, minusculas y digitos")]
+        public string RePassword { get; set; }
         public ActionResult OnGet()
         {
             var idSession = HttpContext.Session.GetString("idSession");
@@ -17,6 +44,19 @@ namespace Sistemadebusqueda2.Pages
             {
                 return RedirectToPage("./Index");
             }
+            return Page();
+        }
+
+
+        public ActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                var password = this.Password;
+                var repassword = this.RePassword;
+                return Page();
+            }
+
             return Page();
         }
 
