244221a225516bda1214a25f2eef0cfa55f4501b
JUAN EDUARDO SEPULVEDA MONARES <JUAN.SEPULVEDAMONA@correoaiep.cl>
2021-09-24 04:32:06 -0300

    2409432

---
+++ Sistemadebusqueda2/Sistemadebusqueda2/Pages/NuevoUsuario.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistemadebusqueda2.Pages
+{
+    public class NuevoUsuarioModel : PageModel
+    {
+        public ActionResult OnGet()
+        {
+            var idSession = HttpContext.Session.GetString("idSession");
+            if (string.IsNullOrEmpty(idSession))
+            {
+                return RedirectToPage("./Index");
+            }
+            return Page();
+        }
+
+     }
+}
---
+++ Sistemadebusqueda2/Sistemadebusqueda2/Pages/Usuarios.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sistemadebusqueda2.Pages
+{
+    public class UsuariosModel : PageModel
+    {
+        public ActionResult OnGet()
+        {
+            var idSession = HttpContext.Session.GetString("idSession");
+            if (string.IsNullOrEmpty(idSession))
+            {
+                return RedirectToPage("./Index");
+            }
+            return Page();
+        }
+    }
+}
